561d434124f4910468c7d543e6eff5dc5cd52693
Ivan Chernov <vanyaz158@gmail.com>
2021-04-19 23:15:41 +0300

    Add callbacks functionality

---
+++ lib/resol/callbacks.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Resol
+  module Callbacks
+    def self.included(othermod)
+      super
+      othermod.extend(ClassMethods)
+      othermod.instance_variable_set(:@__callback_methods__, [])
+    end
+
+    module ClassMethods
+      def inherited(subclass)
+        super
+        subclass.instance_variable_set(:@__callback_methods__, @__callback_methods__.dup)
+      end
+
+      def before_call(method_name)
+        @__callback_methods__ << method_name
+      end
+
+      private
+
+      def __run_callbacks__(instance)
+        @__callback_methods__.each { |method_name| instance.send(method_name) }
+      end
+    end
+  end
+end
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -16,6 +16,20 @@ class EmptyService < Resol::Service
   def call; end
 end
 
+class ServiceWithCallbacks < Resol::Service
+  before_call :define_instance_var
+
+  def call
+    success!(@some_var)
+  end
+
+  private
+
+  def define_instance_var
+    @some_var = "some_value"
+  end
+end
+
 RSpec.describe Resol::Service do
   it "returns a success result" do
     expect(SuccessService.call!).to eq(:success_result)
@@ -33,6 +47,31 @@ RSpec.describe Resol::Service do
     expect { EmptyService.call! }.to raise_error do |error|
       expect(error).to be_a(EmptyService::InvalidCommandImplementation)
       expect(error.message).to eq("No success! or fail! called in the #call method in EmptyService")
+    end
+  end
+
+  context "with defined callbacks" do
+    it "properly exectues them" do
+      expect(ServiceWithCallbacks.call!).to eq("some_value")
+    end
+  end
+
+  context "with inheritance" do
+    let(:other_service) do
+      Class.new(ServiceWithCallbacks) do
+        before_call :set_other_value
+
+        private
+
+        def set_other_value
+          @some_var += "_postfix"
+        end
+      end
+    end
+
+    it "properly handles inheritance" do
+      expect(other_service.call!).to eq("some_value_postfix")
+      expect(ServiceWithCallbacks.call!).to eq("some_value")
     end
   end
 end

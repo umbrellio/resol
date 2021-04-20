ecc6de7c0b0419f876566a502ae1ec265e9708e2
Ivan Chernov <vanyaz158@gmail.com>
2021-04-20 16:37:48 +0300

    Rewrite spec

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -30,6 +30,16 @@ class ServiceWithCallbacks < Resol::Service
   end
 end
 
+class SubServiceWithCallbacks < ServiceWithCallbacks
+  before_call :set_other_value
+
+  private
+
+  def set_other_value
+    @some_var += "_postfix"
+  end
+end
+
 RSpec.describe Resol::Service do
   it "returns a success result" do
     expect(SuccessService.call!).to eq(:success_result)
@@ -50,28 +60,8 @@ RSpec.describe Resol::Service do
     end
   end
 
-  context "with defined callbacks" do
-    it "properly exectues them" do
-      expect(ServiceWithCallbacks.call!).to eq("some_value")
-    end
-  end
-
-  context "with inheritance" do
-    let(:other_service) do
-      Class.new(ServiceWithCallbacks) do
-        before_call :set_other_value
-
-        private
-
-        def set_other_value
-          @some_var += "_postfix"
-        end
-      end
-    end
-
-    it "properly handles inheritance" do
-      expect(other_service.call!).to eq("some_value_postfix")
-      expect(ServiceWithCallbacks.call!).to eq("some_value")
-    end
+  it "properly executes callbacks" do
+    expect(SubServiceWithCallbacks.call!).to eq("some_value_postfix")
+    expect(ServiceWithCallbacks.call!).to eq("some_value")
   end
 end

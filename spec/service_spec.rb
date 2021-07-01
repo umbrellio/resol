d37f7a5614238148cb55712c13c03ddb341fc39b
Ivan <VanyaZ158@gmail.com>
2021-07-01 16:53:44 +0300

    Use Kernel#throw as success interrupter (#7)
    * Remove Success Interruption, use Kernel#throw instead
    
    * Add spec for rollback operation
    
    * Add specs for failure
    
    * More checks
    
    * Remove value from success
    
    * Fix bug with catching thows of other classes
    
    * Fix bugs found in discus

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+class DB
+  class << self
+    attr_accessor :rollbacked
+
+    def transaction
+      self.rollbacked = false
+
+      yield
+      "return_some_val"
+      # rubocop:disable Lint/RescueException
+    rescue Exception
+      # rubocop:enable Lint/RescueException
+      self.rollbacked = true
+      raise
+    end
+  end
+end
+
 class SuccessService < Resol::Service
   def call
     success!(:success_result)
@@ -13,7 +31,9 @@ class FailureService < Resol::Service
 end
 
 class EmptyService < Resol::Service
-  def call; end
+  def call
+    "some_string"
+  end
 end
 
 class ServiceWithCallbacks < Resol::Service
@@ -40,6 +60,27 @@ class SubServiceWithCallbacks < ServiceWithCallbacks
   end
 end
 
+class ServiceWithTransaction < Resol::Service
+  def call
+    DB.transaction { success! }
+  end
+end
+
+class ServiceWithFailInTransaction < Resol::Service
+  def call
+    DB.transaction { fail!(:failed) }
+  end
+end
+
+class HackyService < Resol::Service
+  param :count
+
+  def call
+    success! unless count.zero?
+    HackyService.build(count + 1).call
+  end
+end
+
 RSpec.describe Resol::Service do
   it "returns a success result" do
     expect(SuccessService.call!).to eq(:success_result)
@@ -63,5 +104,32 @@ RSpec.describe Resol::Service do
   it "properly executes callbacks" do
     expect(SubServiceWithCallbacks.call!).to eq("some_value_postfix")
     expect(ServiceWithCallbacks.call!).to eq("some_value")
+  end
+
+  it "doesn't rollback transaction" do
+    result = ServiceWithTransaction.call
+    expect(result.success?).to eq(true)
+    expect(result.value!).to eq(nil)
+    expect(DB.rollbacked).to eq(false)
+  end
+
+  context "when service failed" do
+    it "rollbacks transaction" do
+      result = ServiceWithFailInTransaction.call
+      expect(result.failure?).to eq(true)
+      result.or do |error|
+        expect(error.code).to eq(:failed)
+      end
+
+      expect(DB.rollbacked).to eq(true)
+    end
+  end
+
+  context "when using instance #call inside other service" do
+    let(:expected_message) { /uncaught throw #<HackyService/ }
+
+    it "raises an exception" do
+      expect { HackyService.call!(0) }.to raise_error(UncaughtThrowError, expected_message)
+    end
   end
 end
